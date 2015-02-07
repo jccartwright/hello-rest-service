0fecc88374accda7693e596e5e5233c1bb50fcfb
John Cartwright <john.c.cartwright@noaa.gov>
2015-02-07 09:48:25 -0700

    configure logging

--- src/main/java/hello/web/GreetingController.java
+++ src/main/java/hello/web/GreetingController.java
@@ -1,7 +1,8 @@
 package hello.web;
 
 import java.util.concurrent.atomic.AtomicLong;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import hello.domain.Greeting;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,12 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
-
+    private static Logger logger = LoggerFactory.getLogger(GreetingController.class);
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        logger.debug("inside greeting method with "+name);
         return new Greeting(counter.incrementAndGet(),
                             String.format(template, name));
     }
